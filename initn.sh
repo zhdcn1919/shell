c65e1a62a6618c90a0c9b30be90f8b30fc7a164b
HUAWEI NXT-DL00 <zhdcn1919@163.com>
2018-06-02 15:04:43 +0800

    gmp.sh

--- initn.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-mount -o rw,remount /system
-cp com.google.android.maps.jar /system/framework/ && chmod 644 /system/framework/com.google.android.maps.jar
-cp com.google.android.maps.xml /system/etc/permissions/ && chmod 644 /system/etc/permissions/com.google.android.maps.xml
-mount -o ro,remount /system
-exit 0
